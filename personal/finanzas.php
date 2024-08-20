b00c7dae65ecd19f49e896c5c76e3e838b63bce5
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-08-20 17:48:42 -0500

    formas para personal administrativo

--- personal/finanzas.php
+++ personal/finanzas.php
@@ -102,8 +102,25 @@
 		
 	</div>
 </div>
-
 </div>
-
+<script>
+	const signinBtn = document.querySelector('.signinBtn');
+	const signupBtn = document.querySelector('.signupBtn');
+	const formBx = document.querySelector('.formBx');
+	const cuerpo = document.querySelector('.cuerpo');
+	
+	signupBtn.onclick = function(){
+		formBx.classList.add('active');
+		cuerpo.classList.add('active');
+	}
+	signinBtn.onclick = function(){
+		formBx.classList.remove('active');
+		cuerpo.classList.remove('active');
+	}
+	document.querySelector('#update').onclick=()=>{document.getElementById('nosotros').style.display='block';document.getElementsByTagName('body')[0].style.overflow='hidden';};
+	document.querySelector('#closeModal').onclick=()=>{document.getElementById('nosotros').style.display='none';document.getElementsByTagName('body')[0].style.overflow='visible';}
+	
+	document.querySelectorAll('.updateForm').forEach(input=> input.disabled=true);
+</script>
 </body>
 </html>
