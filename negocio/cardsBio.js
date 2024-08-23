ff37b8a7647a8002729dd84821d47727c58354f7
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-08-23 00:40:53 -0500

    actualizacion del sitio

--- negocio/cardsBio.js
+++ negocio/cardsBio.js
@@ -6,20 +6,19 @@ const consultaBio = async() =>{
 	const data = await res.json();
 };
 const insertaBio = productos =>{
-	productos.forEach( producto=>{
+	productos.forEach( producto =>{
 		if(producto.clase === 'bio'){
-		productosBio = `<div class="boxEco">
-							<span></span>
-							<div class='contentEco'>
-								<img src="${producto.imProd}" alt="cargando imagen de ${producto.title}">
-								<h2>${producto.title}</h2>
-								<p>${producto.description}
-									</p>
-								<a target='_blank' href='${producto.url}'>Compra en Amazon</a>
-							</div>
-						</div>`;
+			productosBio += `<div class="boxEco">
+								<span></span>
+								<div class='contentEco'>
+									<img src="${producto.imProd}" alt="cargando imagen de ${producto.title}">
+									<h2>${producto.title}</h2>
+									<p>${producto.description}
+										</p>
+									<a target='_blank' href='${producto.url}'>Compra en Amazon</a>
+								</div>
+							</div>`;
 		}
-		
-	});
 	contenidoBioEco.innerHTML = productosBio;
+	});
 };
