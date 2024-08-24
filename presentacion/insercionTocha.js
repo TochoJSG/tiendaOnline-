b6f25835ed25628417b5ff853231f8d6292868ff
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-08-24 18:17:09 -0500

    cambios de estilo a index

--- presentacion/insercionTocha.js
+++ presentacion/insercionTocha.js
@@ -48,19 +48,19 @@ const pintarCards = data =>{
 const pintarCarrusel = data =>{
 	let plantilla2 = '';
 	data.forEach(item=>{
-	if( (item.clase=='matprimas') || (item.plataforma=='ml') ){
-	plantilla2 += `<div class="prod_car">
-				<picture>
-					<div class="imgBx">
-					<img src="${item.imProd}" alt="loading..." />
-					</div>
-				</picture>
-				<div class="details_car">
-					<h2>${item.title}</h2>
-					<a target="_blank" href="${item.url}">Ver en Amazon</a>
-				</div>
-			</div>`;
-	}
+		if( (item.clase==='matprimas') || (item.plataforma==='ml') ){
+			plantilla2 += `<div class="prod_car">
+						<picture>
+							<div class="imgBx">
+							<img src="${item.imProd}" alt="loading..." />
+							</div>
+						</picture>
+						<div class="details_car">
+							<h2>${item.title}</h2>
+							<a target="_blank" href="${item.url}">Ver en Amazon</a>
+						</div>
+					</div>`;
+		}
 	});
 	carrusel.innerHTML = plantilla2;
 }
