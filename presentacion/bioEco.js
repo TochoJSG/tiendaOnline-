cfc5d65bc9f6637a96e5fc35fd40d8f62109f1ff
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-08-23 21:03:22 -0500

    modificaciones a estilos

--- presentacion/bioEco.js
+++ presentacion/bioEco.js
@@ -14,9 +14,9 @@ const pintarCards = data =>{
 	data.forEach(item=>{
 		if(item.clase==="bio"){   
 			plantilla1 += `<div class="prod_car">
-				<picture>
+				<div class="picture">
 					<img class="imgBx" src="${item.imBase}">
-				</picture>
+				</div>
 				<div class="details_car">
 						<img src="${item.imProd}" alt="Error al cargar imagen de ${item.title}"/>
 						<h2>${item.title}</h2>
