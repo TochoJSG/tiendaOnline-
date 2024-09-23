08b427332ad885c48fbc7ee78b854743fca4ac87
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-09-23 12:21:02 -0500

    interfaz para el personal

--- personal/personal.php
+++ personal/personal.php
@@ -7,120 +7,20 @@
 	<title>Efect Nav</title>
     <style>
 	@import url('https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap');
-	*{
-		box-sizing:border-box;
-		margin:0;
-		padding:0;
-		font-family:'Poppins',sans-serif;
-		scroll-behavior:smooth;
-	}
-    body{
-		display:flex;
-		justify-content:center;
-		align-items:center;
-		min-height:100vh;
-		background:#040105;
-		width:100%;
-		overflow:hidden;
-	}
-	ul{
-		position:relative;
-		display:flex;
-		flex-direction:column;
-	}
-	ul li{
-		list-style:none;width:666px;
-	}
-	ul li:before{
-		content:attr(data-text);
-		position:absolute;
-		top:50%;
-		left:50%;
-		transform:translate(-50%,-50%);
-		color:#fff;
-		font-size:16em;
-		font-weight:700;
-		pointer-events:none;
-		opacity:0;
-		letter-spacing:17px;
-		transition:0.666s;
-		white-space:nowrap;
-	}
-	ul li:hover:before{
-		opacity:0.0666;
-		letter-spacing:0px;
-	}
-	ul li a{
-		position:relative;
-		
-		display:inline-block;
-		padding:6px 15px;
-		text-decoration:none;
-		background:#2196f3;
-		color:#fff;
-		text-transform:uppercase;
-		letter-spacing:3px;
-		overflow:hidden;
-		transition:0.666s;
-		z-index:1;
-	}
-	ul:hover li a{
-		opacity:0;
-	}
-	ul li a:hover{
-		transform:scale(1.1);
-		z-index:1000;
-		background:#daf;
-		opacity:1;
-	}
-	.btnAnimaLuz{
-			display: flex;
-			position:relative;
-			justify-content:center;
-			align-items:center;
-			flex-wrap:wrap;
-			margin:17px 0;
-		}
-		.btnAnimaLuz a{
-			position:relative;
-			width:100%;
-			height:50px;
-			margin:16px;
-			line-height:48px;
-			background:#000;
-			text-transform:uppercase;
-			font-size:1.3em;
-			letter-spacing:5px;
-			text-decoration:none;			
-			-webkit-box-reflect:below 1px linear-gradient(transparent,#0004);
-		}
-		.btnAnimaLuz a span{
-			position:absolute;
-			display:block;
-			top:1px;
-			left:1px;
-			right:1px;
-			bottom:1px;
-			text-align:center;
-			background:#0c0c0c;
-			color:rgba(255,255,255,0.2);
-			transition:0.6s;
-			z-index:1;
-		}
-		.btnAnimaLuz a::after{
-			content:'';
-			position:absolute;
-			top:0;
-			left:0;
-			width:100%;
-			height:100%;
-			background:linear-gradient(45deg,#fb0094,#00f,#0f0,#ff0,#f00,#fb0094,#00f,#0f0,#ff0,#f00);
-			background-size:400%;
-			opacity:0;
-			filter:blur(20px);
-			transition:0.6s;
-			animation:animate 20s linear infinite;
-		}
+	*{box-sizing:border-box;margin:0;padding:0;font-family:'Poppins',sans-serif;scroll-behavior:smooth;}
+    body{display:flex;justify-content:center;align-items:center;min-height:100vh;background:#040105;width:100%;overflow:hidden;}
+	ul{position:relative;display:flex;flex-direction:column;}
+	ul li{list-style:none;width:666px;}
+	ul li:before{content:attr(data-text);position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);color:#fff;font-size:16em;font-weight:700;pointer-events:none;opacity:0;letter-spacing:17px;transition:0.666s;white-space:nowrap;}
+	ul li:hover:before{opacity:0.0666;letter-spacing:0px;}
+	ul li a{position:relative;display:inline-block;padding:6px 15px;text-decoration:none;background:#2196f3;color:#fff;text-transform:uppercase;letter-spacing:3px;overflow:hidden;transition:0.666s;z-index:1;}
+	ul:hover li a{opacity:0;}
+	ul li a:hover{transform:scale(1.1);z-index:1000;background:#daf;opacity:1;}
+	.btnAnimaLuz{display:flex;position:relative;justify-content:center;align-items:center;flex-wrap:wrap;margin:17px 0;}
+    .btnAnimaLuz a{position:relative;width:100%;height:50px;margin:16px;line-height:48px;background:#000;text-transform:uppercase;font-size:1.3em;letter-spacing:5px;text-decoration:none;			
+			-webkit-box-reflect:below 1px linear-gradient(transparent,#0004);}
+    .btnAnimaLuz a span{position:absolute;display:block;top:1px;left:1px;right:1px;bottom:1px;text-align:center;background:#0c0c0c;color:rgba(255,255,255,0.2);transition:0.6s;z-index:1;}
+	.btnAnimaLuz a::after{content:'';position:absolute;top:0;left:0;width:100%;height:100%;background:linear-gradient(45deg,#fb0094,#00f,#0f0,#ff0,#f00,#fb0094,#00f,#0f0,#ff0,#f00);background-size:400%;opacity:0;filter:blur(20px);transition:0.6s;animation:animate 20s linear infinite;}
 		.btnAnimaLuz a::before{
 			content:'';
 			position:absolute;
@@ -165,13 +65,13 @@
 <body>
 <ul>
 	<li data-text="Inventario" class="btnAnimaLuz">
-		<a href="#"><span>Gestionar Inventario</span></a>
+		<a target="_self" href="procesos/GestionInventario.php"><span>Gestionar Inventario</span></a>
 		</li>
 	<li data-text="Personal" class="btnAnimaLuz">
-		<a href="#"><span>Gestionar Empleados</span></a>
+		<a target="_self" href="procesos/GestionPersonal.php"><span>Gestionar Empleados</span></a>
 		</li>
 	<li data-text="Finanzas" class="btnAnimaLuz">
-		<a href="#"><span>Gestionar Finanzas</span></a>
+		<a target="_self" href="procesos/GestionFinanzas.php"><span>Gestionar Finanzas</span></a>
 		</li>
 </ul>
 <script>
