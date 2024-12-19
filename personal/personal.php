c14ca247cc11e6cdb78a65ba3ce295c0a789f416
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-12-19 17:19:15 -0600

    error

--- personal/personal.php
+++ personal/personal.php
@@ -71,7 +71,7 @@
 <script>
 	const menuToggle=document.querySelector('.menuToggle');
 	const navigation=document.querySelector('.navigation');
-	menuToggle.onclick=function(){
+	menuToggle.onClick=function(){
 		menuToggle.classList.toggle('active');
 		navigation.classList.toggle('active');
 	}
