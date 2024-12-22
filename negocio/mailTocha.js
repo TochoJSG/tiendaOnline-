eb6f31319081b279c6097a9b129df82566341185
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-12-21 21:29:22 -0600

    procesos internos

--- negocio/mailTocha.js
+++ negocio/mailTocha.js
@@ -32,10 +32,12 @@ async function sendMsg(e) {
 		const result = await response.json();
 
 		if (response.ok) {
-			alert(result.message || 'Mensaje enviado con éxito.');
+			//alert(result.message || 'Mensaje enviado con éxito.');
+			alert('Mensaje enviado con éxito.');
 			form.reset();
 		} else {
-			alert(result.error || 'Hubo un error al enviar el mensaje.');
+			//alert(result.error || 'Hubo un error al enviar el mensaje.');
+			alert('Hubo un error al enviar el mensaje.');
 		}
 	} catch (error) {
 		alert('Error en la conexión al servidor.');
