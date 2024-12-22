d086cba83e888f8e8532abb6816297785954b329
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-12-21 22:25:30 -0600

    procesos internos

--- personal/registraEmpleado.php
+++ personal/registraEmpleado.php
@@ -14,13 +14,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $telefono = filter_input(INPUT_POST, 'tel', FILTER_SANITIZE_STRING);
         $email = filter_input(INPUT_POST, 'mail', FILTER_SANITIZE_EMAIL);
         $rfc = filter_input(INPUT_POST, 'rfc', FILTER_SANITIZE_STRING);
-        $fecha_ingreso = filter_input(INPUT_POST, 'ingreso', FILTER_SANITIZE_STRING);
+        $fecha_ingreso = filter_input(INPUT_POST, 'fechaIngreso', FILTER_SANITIZE_STRING);
         $departamento = filter_input(INPUT_POST, 'departamento', FILTER_VALIDATE_INT);
         $rol = filter_input(INPUT_POST, 'rol', FILTER_VALIDATE_INT);
         $contrato = filter_input(INPUT_POST, 'contrato', FILTER_VALIDATE_INT);
 
         if ($nombre && $apellidos && $sueldo && $telefono && $rfc && $fecha_ingreso && $departamento && $rol && $contrato) {
-            $sql = $conn->prepare("CALL insertarEmpleado(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $sql = $conn->prepare("CALL insertarEmpleado(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
             $sql->execute([$nombre, $apellidos, $sueldo, $telefono, $email, $rfc, $fecha_ingreso, $departamento, $rol, $contrato]);
 
             echo '<script>alert("Empleado registrado exitosamente."); window.location.href="./";</script>';
