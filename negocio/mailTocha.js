9924477dfe7f0aed98aadee778e1578933f4ce25
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-12-22 17:49:08 -0600

    procesos internos refinamiento

--- negocio/mailTocha.js
+++ negocio/mailTocha.js
@@ -17,41 +17,8 @@ function sendMsg(e){
 }
 forma.addEventListener('submit',sendMsg);*/
 
-
-/*
 const form = document.querySelector('.contactoGMAIL');
 
-async function sendMsg(e) {
-	e.preventDefault();
-
-	const formData = new FormData(form);
-
-	try {
-		const response = await fetch('./enviar_mail.php', {
-			method: 'POST',
-			body: formData
-		});
-
-		const result = await response.json();
-
-		if (response.ok) {
-			alert(result.message || 'Mensaje enviado con éxito.');
-			//alert('Mensaje enviado con éxito.');
-			form.reset();
-		} else {
-			alert(result.error || 'Hubo un error al enviar el mensaje.');
-			//alert('Hubo un error al enviar el mensaje.');
-		}
-	} catch (error) {
-		alert('Error en la conexión al servidor.');
-	}
-}
-
-form.addEventListener('submit', sendMsg);*/
-
-const form = document.querySelector('.contactoGMAIL');
-
-// Función asíncrona para manejar el envío del formulario
 async function sendMsg(e) {
     e.preventDefault(); // Evita el envío predeterminado del formulario
 
@@ -62,7 +29,7 @@ async function sendMsg(e) {
 		headers: {
 			'Content-Type':'application/json'
 		},
-        body: formData.json()
+        body: formData
 	};
 
 	fetch('./enviar_mail.php', opciones)
