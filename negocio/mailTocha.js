58585f03a1bb0ea6195e3fe5cf59fc1814bc5e17
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2024-12-22 10:50:59 -0600

    procesos internos refinamiento

--- negocio/mailTocha.js
+++ negocio/mailTocha.js
@@ -1,21 +1,23 @@
-/*const forma=document.querySelector('.contactoGMAIL');
+const forma=document.querySelector('.contactoGMAIL');
 function sendMsg(e){
 	e.preventDefault();
 	const nombre=document.querySelector('.lname'),
-		correo=document.querySelector('.mail'),
-		telefono=document.querySelector('.tel'),
-		msj=document.querySelector('.msg');		
-		Email.send({
-			SecureToken:"c40454ce-123a-49b8-93e9-ed83ec251b1f",
-			To:'matprimas.tocha.loc33@gmail.com',
-			From:correo.value,
-			Subject:"Contacto G-Tocha Tocha",
-			Body:msj.value
-		}).then(
-		  message=>alert(message)
-		);
-	}
-forma.addEventListener('submit',sendMsg);*/
+	correo=document.querySelector('.mail'),
+	telefono=document.querySelector('.tel'),
+	msj=document.querySelector('.msg');		
+	Email.send({
+		SecureToken:"c40454ce-123a-49b8-93e9-ed83ec251b1f",
+		To:'matprimas.tocha.loc33@gmail.com',
+		From:correo.value,
+		Subject:"Contacto G-Tocha Tocha",
+		Body:msj.value
+	}).then(
+		message=>alert(message)
+	);
+}
+forma.addEventListener('submit',sendMsg);
+
+/*
 const form = document.querySelector('.contactoGMAIL');
 
 async function sendMsg(e) {
@@ -44,4 +46,4 @@ async function sendMsg(e) {
 	}
 }
 
-form.addEventListener('submit', sendMsg);
+form.addEventListener('submit', sendMsg);*/
